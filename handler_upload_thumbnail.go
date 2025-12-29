0880d5dc968d0cae554c9373ecf1ed964b1b5858
CodeZeroSugar <icvelder@gmail.com>
2025-12-29 14:22:24 -0600

    implemented thumbnail update handler

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,42 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+	media := header.Header.Get("Content-Type")
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to read file into bytes", err)
+		return
+	}
+	metaData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to get video from database", err)
+		return
+	}
+	if metaData.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Authenticated user is not the video owner", err)
+		return
+	}
+	thumb := thumbnail{
+		data:      bytes,
+		mediaType: media,
+	}
+	videoThumbnails[videoID] = thumb
+	url := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	metaData.ThumbnailURL = &url
+	if err = cfg.db.UpdateVideo(metaData); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to update video with new thumbnail URL", err)
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	respondWithJSON(w, http.StatusOK, metaData)
 }
